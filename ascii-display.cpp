2c2f515dc3ba54961fc7b4b0e3dee2972882b19a
erdemy885 <erdemy885@gmail.com>
2023-12-18 03:11:22 -0500

    windows support

--- ascii-display.cpp
+++ ascii-display.cpp
@@ -1,9 +1,34 @@
 #include "ascii-display.h"
-#include <ncurses.h>
-#include <string>
-using namespace std;
 
-const string brightness = " .,-~:;=!*#$@";
+#if defined(_WIN32)
+
+#include <windows.h>
+
+Window::Window(int x, int y){
+    width = x;
+    height = y;
+    screen = new char[width*height];
+    cls();
+	SetConsoleActiveScreenBuffer(hConsole);
+}
+
+void Window::draw(int x, int y, int b){
+    screen[y*width + x] = brightness[b];
+}
+
+void Window::ref(){
+    WriteConsoleOutputCharacterA(hConsole, screen, width * height, { 0,0 }, &dwBytesWritten);
+}
+
+void Window::cls(){
+    for (int i = 0; i < width*height; i++) {
+        screen[i] = ' ';
+    }
+}
+
+#elif defined(__APPLE__) || defined(__linux__)
+
+#include <ncurses.h>
 
 Window::Window(int x, int y)
 {
@@ -32,3 +57,5 @@ void Window::cls()
 {
     erase();
 }
+
+#endif
--- ascii-display.h
+++ ascii-display.h
@@ -1,6 +1,11 @@
 #ifndef ASCII_DISPLAY_H
 #define ASCII_DISPLAY_H
 
+#include <string>
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
 class Window
 {
 public:
@@ -9,6 +14,13 @@ public:
     void ref();
     void cls();
     int width, height;
+private:
+    const std::string brightness = " .,-~:;=!*#$@";
+#if defined(_WIN32)
+    char *screen;
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    DWORD dwBytesWritten = 0;
+#endif
 };
 
 #endif
